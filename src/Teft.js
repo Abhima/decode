104c3d9528b1b9af11b03c66f1d071ccc4f0e462
Abhima <monika@github.com>
2020-08-24 13:16:45 +0200

    just added homepage image

--- src/Teft.js
+++ src/Teft.js
@@ -1,12 +1,35 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Grid, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(15),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export const Teft = () => {
+  const classes = useStyles();
   return (
     <div className="headerTitle">
-      <Typography variant="h1" gutterBottom>
-        This is a Home page.
-      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h2" gutterBottom className={classes.paper}>
+            The beauty of colors and figures.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <img
+            src={require("./assets/Images/see.png")}
+            className="img-fluid"
+            alt=""
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 };
